cc2df73ba2a5c4f25a8975c64fdc492e9a339990
sergiocaronte <sjuniorhp@gmail.com>
2015-12-15 15:39:49 -0200

    disclaimer added.

--- src/raytracer.cpp
+++ src/raytracer.cpp
@@ -1,3 +1,19 @@
+/*
+* Copyright (C) 2015 Sergio Nunes da Silva Junior 
+*
+* This program is free software; you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the Free
+* Software Foundation; either version 2 of the License.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+* more details.
+*
+* author: Sergio Nunes da Silva Junior
+* contact: sergio.nunes@dcc.ufmg.com.br
+* Universidade Federal de Minas Gerais (UFMG) 
+*/
 #include "raytracer.h"
 #include "ppmimage.h"
 #include <fstream>
